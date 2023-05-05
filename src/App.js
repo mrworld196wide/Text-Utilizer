65cd47e55c9ea8a65c6ae9e7e3a686168f2a9681
mrworld196wide <shubhum19@gmail.com>
2023-05-05 17:13:33 +0530

    Improving Dark Mode & Refactoring App Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 
 function App() {
+  //state hook which is to used to toggle between light mode and dark mode
+  const [mode, setMode] = useState('dark'); 
+
+  // creating function to update state mode
+  const toggleMode=() =>{
+    if (mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor= '#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor= 'white';
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" />
+      {/* darkmode is sent as props to Navbar component */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        {/* <Textform heading="Enter your text below" /> */}
-        <About/>
+        <Textform heading="Enter your text below" mode={mode}/>
+        {/* <About/> */}
       </div>
     </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -41,10 +41,12 @@ export default function Textform(props) {
     const [text, setText] = useState('enter text here');
     return (
         <>
-            <div className="mb-3">
+            <div className="mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className='mb-3'>
-                    <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+                    <textarea className="form-control" id="myBox" rows="8" value={text} 
+                    onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', 
+                    color: props.mode === 'dark' ? 'white' : 'black' }}></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
@@ -53,16 +55,16 @@ export default function Textform(props) {
                 <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
             </div>
 
-            <div className="d-inline container my-4 ">
+            <div className="d-inline container my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <p className=''>{value}</p>
                 <button className='btn btn-danger' onClick={() => setValue((value + 1))}>Increment Value</button>
             </div>
-            <div className="container my-2">
+            <div className="container my-2" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>Your text summary</h1>
                 <p> {text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} minutes to read </p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0? text:"enter your text to preview"}</p>
             </div>
         </>
     )
