671da5ff3fdf4c4d1d48bf85b7be5e902fa9127a
mrworld196wide <shubhum19@gmail.com>
2023-05-05 19:12:53 +0530

    Adding + Auto Dismissing Alert Messages

--- src/App.js
+++ src/App.js
@@ -4,28 +4,46 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import Alert from './components/Alert';
 
 function App() {
   //state hook which is to used to toggle between light mode and dark mode
-  const [mode, setMode] = useState('dark'); 
+  const [mode, setMode] = useState('light'); 
+
+  // creeating state for alert component
+  const [alert, setAlert] = useState(null);
+
+  // method for alert
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
 
   // creating function to update state mode
   const toggleMode=() =>{
     if (mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor= '#042743';
+      showAlert("Darkmode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor= 'white';
+      showAlert("Lightmode has been enabled", "success");
     }
   }
   return (
     <>
       {/* darkmode is sent as props to Navbar component */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <Textform heading="Enter your text below" mode={mode}/>
+        <Textform showAlert={showAlert} heading="Enter your text below" mode={mode}/>
         {/* <About/> */}
       </div>
     </>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -4,18 +4,21 @@ export default function Textform(props) {
         console.log("Uppercased successully!");
         let newUpText = text.toUpperCase();      //uppercasing the text
         setText(newUpText); //updating the statevariable via state to new uppercased text
+        props.showAlert("Converted to uppercase", "success"); //sends an alert prompt using alert component
     }
 
     const handleLowClick = () => {
         console.log("Lowercased successully!");
         let newUpText = text.toLowerCase();      //lowercasing the text
         setText(newUpText); //updating the statevariable via state to new lowercased text
+        props.showAlert("Converted to lowercase", "success"); //sends an alert prompt using alert component
     }
 
     const handleClearClick = () => {
         console.log("textbox cleared")
         let newText = " ";
         setText(newText)
+        props.showAlert("Textbox cleared", "success"); //sends an alert prompt using alert component
     }
 
     const handleOnChange = (event) => {
@@ -29,11 +32,13 @@ export default function Textform(props) {
         text.select();
         text.setSelectionRange(0, 9999);
         navigator.clipboard.writeText(text.value);
+        props.showAlert("Copied successfully", "success"); //sends an alert prompt using alert component
     }
 
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "))
+        props.showAlert("Extra spaces removed", "success"); //sends an alert prompt using alert component
     }
 
     // states
@@ -44,9 +49,7 @@ export default function Textform(props) {
             <div className="mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h1>{props.heading}</h1>
                 <div className='mb-3'>
-                    <textarea className="form-control" id="myBox" rows="8" value={text} 
-                    onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', 
-                    color: props.mode === 'dark' ? 'white' : 'black' }}></textarea>
+                    <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}}></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
@@ -64,7 +67,7 @@ export default function Textform(props) {
                 <p> {text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").length} minutes to read </p>
                 <h2>Preview</h2>
-                <p>{text.length>0? text:"enter your text to preview"}</p>
+                <p>{text.length > 0 ? text : "enter your text to preview"}</p>
             </div>
         </>
     )
